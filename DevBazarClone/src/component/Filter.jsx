4cc3fbf44b817ac2ecfb116421b9cf8823521c93
Kanishk Gupta <kanishkgupta1234@gmail.com>
2024-04-11 23:36:10 +0530

    fix search

--- DevBazarClone/src/component/Filter.jsx
+++ DevBazarClone/src/component/Filter.jsx
@@ -1,147 +1,148 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function Filter({ array, flag, newarray, setitem, filtereditems }) {
-  const [searchTerm, setSearchTerm] = useState("");
-  const json = [
-    { value: 1, text: "Low to High" },
-    { value: 2, text: "High to Low" },
-  ];
-
-  const Sort = (e) => {
-    const value = e.target.value;
-    console.log(value);
-    if (value == 1) {
-      filtereditems.sort((a, b) => {
-        return a.price - b.price;
-      });
-
-      setitem([...filtereditems]);
-    } else {
-      filtereditems.sort((a, b) => {
-        return b.price - a.price;
-      });
-      setitem([...filtereditems]);
-    }
-
-    console.log("Array sorted");
-  };
-
-  const SearchFilter = (e) => {
-    const value = e.target.value;
-    console.log(value);
-
-    const data = array.filter((item) => {
-      return item.attributes.brand === value;
-    });
-
-    newarray(data);
-  };
-  // const debouce = (func, timer) => {
-  //   let timerid;
-  //   return function (...args) {
-  //     clearTimeout(timerid);
-  //     timerid = setTimeout(() => {
-  //       func();
-  //       console.log("inside debounce function\n");
-  //     }, timer);
-  //   };
-  // };
-
-  const debounce = (func, delay) => {
+const debounce = (func, delay) => {
     let timeoutId;
     return function (...args) {
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => {
-        func.apply(this, args);
-      }, delay);
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
     };
-  };
+};
 
-  const handleSearchChange = (e) => {
-    const value = e.target.value;
+const json = [
+    { value: 1, text: "Low to High" },
+    { value: 2, text: "High to Low" },
+];
 
-    setSearchTerm(value);
-    const data = filtereditems.filter((item) => {
-      return item.attributes.brand == value;
-    });
-    console.log(`Array ==========`);
-    console.log(array);
-    console.log(`newArray ==========`);
-    console.log(filtereditems);
+function Filter({ initialItems = [], setFilteredData }) {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [sortValue, setSortValue] = useState(json[0].value);
 
-    newarray(data);
+    useEffect(() => {
+        let initialData = [...initialItems];
+        if (searchTerm) {
+            initialData = initialData.filter((data) =>
+                data.attributes.brand
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase())
+            );
+        }
 
-    console.log(`Array ==========`);
-    console.log(array);
-    console.log(`newArray ==========`);
-    console.log(filtereditems);
-  };
+        initialData.sort((a, b) => {
+            if (Number(sortValue) === 1) {
+                return a.price - b.price;
+            } else return b.price - a.price;
+        });
 
-  const debouncedSearchChange = debounce(handleSearchChange, 300);
-  return (
-    <>
-      <div className="outline-1 outline-slate-200 outline	col-span-2 h-80 p-5 m-2">
-        <div className="flex flex-row justify-between m-1 font-normal">
-          <span className="flex m-0 mb-1 ">Filter</span>
-          <span className="flex cursor-pointer ">Clear All</span>
-        </div>
-        <div className=" outline outline-slate-100 outline-1 "></div>
-        <div className="flex mt-2 flex-row justify-center m-1  font-normal">
-          <input
-            onChange={debouncedSearchChange}
-            className="flex  p-1 w-96 border "
-            placeholder="Search for Product"
-          />
-        </div>
+        setFilteredData([...initialData]);
+    }, [searchTerm, sortValue]);
 
-        <div className="container mt-1 mb-1 ">
-          <div className=" font-normal">Sort By:</div>
-          <div className="flex flex-col">
-            {json.map((entry) => {
-              return (
-                <>
-                  {" "}
-                  <div className="flex-col font-thin">
-                    <label className="font-thin">
-                      <input
-                        key={entry.value}
-                        onChange={Sort}
-                        type="radio"
-                        name="Radiogroup"
-                        value={entry.value}
-                      />{" "}
-                      {entry.text}
-                    </label>
-                  </div>
-                </>
-              );
-            })}
-          </div>
-        </div>
+    const Sort = (e) => {
+        const value = e.target.value;
+        setSortValue(value);
+    };
 
-        <div className="container mt-1 mb-1 ">
-          <div className=" font-normal text-base">Brands</div>
-          <div className="container">
-            <div className="flex-col">
-              <label className="  flex font-thin">
-                <input type="checkbox" name="Radiogroup" value="Levis" /> Levis
-              </label>
-              <label className="  flex font-thin">
-                <input type="checkbox" name="Radiogroup" value="HRX" /> HRX
-              </label>
-              <label className="  flex font-thin">
-                <input type="checkbox" name="Radiogroup" value="Only" /> Only
-              </label>
-              <label className=" flex flex font-thin">
-                <input type="checkbox" name="Radiogroup" value="Nautica" />{" "}
-                Nautica
-              </label>
+    // const SearchFilter = (e) => {
+    //     const value = e.target.value;
+    //     console.log(value);
+
+    //     const data = initialItems.filter((item) => {
+    //         return item.attributes.brand === value;
+    //     });
+
+    //     setFilteredData(data);
+    // };
+
+    const handleSearchChange = (e) => {
+        const value = e.target.value;
+        setSearchTerm(value);
+    };
+    const debouncedSearchChange = debounce(handleSearchChange, 300);
+
+    return (
+        <>
+            <div className="outline-1 outline-slate-200 outline	col-span-2 h-80 p-5 m-2">
+                <div className="flex flex-row justify-between m-1 font-normal">
+                    <span className="flex m-0 mb-1 ">Filter</span>
+                    <span className="flex cursor-pointer ">Clear All</span>
+                </div>
+                <div className=" outline outline-slate-100 outline-1 "></div>
+                <div className="flex mt-2 flex-row justify-center m-1  font-normal">
+                    <input
+                        onChange={debouncedSearchChange}
+                        className="flex  p-1 w-96 border "
+                        placeholder="Search for Product"
+                    />
+                </div>
+
+                <div className="container mt-1 mb-1 ">
+                    <div className=" font-normal">Sort By:</div>
+                    <div className="flex flex-col">
+                        {json.map((entry) => {
+                            return (
+                                <>
+                                    {" "}
+                                    <div className="flex-col font-thin">
+                                        <label className="font-thin">
+                                            <input
+                                                key={entry.value}
+                                                onChange={Sort}
+                                                type="radio"
+                                                name="Radiogroup"
+                                                value={entry.value}
+                                            />{" "}
+                                            {entry.text}
+                                        </label>
+                                    </div>
+                                </>
+                            );
+                        })}
+                    </div>
+                </div>
+
+                <div className="container mt-1 mb-1 ">
+                    <div className=" font-normal text-base">Brands</div>
+                    <div className="container">
+                        <div className="flex-col">
+                            <label className="  flex font-thin">
+                                <input
+                                    type="checkbox"
+                                    name="Radiogroup"
+                                    value="Levis"
+                                />{" "}
+                                Levis
+                            </label>
+                            <label className="  flex font-thin">
+                                <input
+                                    type="checkbox"
+                                    name="Radiogroup"
+                                    value="HRX"
+                                />{" "}
+                                HRX
+                            </label>
+                            <label className="  flex font-thin">
+                                <input
+                                    type="checkbox"
+                                    name="Radiogroup"
+                                    value="Only"
+                                />{" "}
+                                Only
+                            </label>
+                            <label className=" flex flex font-thin">
+                                <input
+                                    type="checkbox"
+                                    name="Radiogroup"
+                                    value="Nautica"
+                                />{" "}
+                                Nautica
+                            </label>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
 
 export default Filter;
--- DevBazarClone/src/component/Wrapper.jsx
+++ DevBazarClone/src/component/Wrapper.jsx
@@ -5,52 +5,43 @@ import { useState } from "react";
 import axios from "axios";
 
 const Wrapper = () => {
-  // const [val, setVal] = useState();
-  const [items, setitem] = useState([]);
-  const [filtereditems, setfiltereditem] = useState([]);
-  const [filter, setfilter] = useState("false");
-  const [name, setName] = useState();
-  const fetchdata = async () => {
-    try {
-      const response = await axios.get(
-        "https://gkanishk-devbazar-be.vercel.app/products"
-      );
-      setitem(response.data.response);
+    const [items, setItem] = useState([]);
+    const [filtereditems, setfiltereditem] = useState([]);
 
-      setfiltereditem(response.data.response);
-    } catch (error) {
-      console.error("Error fetching products:", error);
-      throw error;
-    }
-  };
-  useEffect(() => {
-    fetchdata();
-  }, []);
+    const fetchdata = async () => {
+        try {
+            const response = await axios.get(
+                "https://gkanishk-devbazar-be.vercel.app/products"
+            );
 
-  useEffect(() => {
-    setitem(filtereditems);
-  }, [filtereditems]);
+            setItem(response.data.response);
+            setfiltereditem(response.data.response);
+        } catch (error) {
+            console.error("Error fetching products:", error);
+        }
+    };
 
-  return (
-    <div className="container pt-10 grid">
-      <div className="grid grid-cols-8 p-1 gap-10 m-2">
-        <Filter
-          array={items}
-          newarray={setfiltereditem}
-          flag={setfilter}
-          setitem={setitem}
-          filtereditems={filtereditems}
-        />
-        <div className="col-span-6 h-full p-5 m-2">
-          <div className="grid grid-cols-4">
-            {filtereditems.map((item) => {
-              return <Card key={item.id} item={item} />;
-            })}
-          </div>
+    useEffect(() => {
+        fetchdata();
+    }, []);
+
+    return (
+        <div className="container pt-10 grid">
+            <div className="grid grid-cols-8 p-1 gap-10 m-2">
+                <Filter
+                    initialItems={items}
+                    setFilteredData={setfiltereditem}
+                />
+                <div className="col-span-6 h-full p-5 m-2">
+                    <div className="grid grid-cols-4">
+                        {filtereditems.map((item) => {
+                            return <Card key={item.id} item={item} />;
+                        })}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Wrapper;
